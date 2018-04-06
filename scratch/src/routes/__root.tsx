ee318bc07fc38ceb364e1cb7845ad449fae08efc
ThiagoRAD <thauroo@gmail.com>
2018-04-06 10:39:18 -0300

    __root

--- scratch/src/routes/__root.tsx
+++ scratch/src/routes/__root.tsx
@@ -1,15 +1,44 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+// src/routes/__root.tsx
+/// <reference types="vite/client" />
+import type {ReactNode} from 'react';
+import {Outlet, createRootRoute, HeadContent, Scripts} from '@tanstack/react-router';
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+  }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
+    <RootDocument>
       <Outlet />
-    </React.Fragment>
-  )
+    </RootDocument>
+  );
+}
+
+function RootDocument({children}: Readonly<{children: ReactNode}>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
 }
